ae7ad160acc02fd303f02c73ee12148f2e1eaf9b
wutao <363697339@qq.com>
2019-08-11 23:13:30 +0800

    modify

--- builds.py
+++ builds.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 import sys
@@ -7,6 +6,7 @@ import getopt
 
 from xml.dom.minidom import parse
 import xml.dom.minidom
+
 
 
 cwd = None
@@ -22,11 +22,10 @@ debug_tag= ""
 std_tag = "" 
 macro_list = [] 
 
-def linux_cmd(cmd):
-    return os.system(cmd)
+function_mapping = dict()
 
-def windows_cmd():
-    pass
+def execute_cmd(cmd):
+    return os.system(cmd)
 
 def dir_make(base_dir,sub_dir):
     target_dir = os.path.join(base_dir,sub_dir)
@@ -40,7 +39,7 @@ def dir_make(base_dir,sub_dir):
 def detect(prop_config):
     global cc
     cc=prop_config["cc"]
-    ret_val = linux_cmd("which %s" % cc)
+    ret_val = execute_cmd("which %s" % cc)
     if ret_val is not 0:
         print("CC: %s not found" % cc)
         return 
@@ -57,9 +56,6 @@ def detect(prop_config):
     obj_dir = prop_config["obj-dir"]
     dir_make(cwd,obj_dir)
 
-def config():
-    pass
-
 def make_executable(obj_list,target_name,lib_list,lib_dir_list):
     objects_tag = " ".join(obj_list)
 
@@ -68,7 +64,7 @@ def make_executable(obj_list,target_name,lib_list,lib_dir_list):
     lib_dirs_tag = " ".join(["-L%s" % d for d in lib_dir_list])
 
     link_cmd="%s -o %s/%s %s %s %s" % (cc,dist_dir,target_name,objects_tag,lib_dirs_tag,libs_tag)
-    linux_cmd(link_cmd)
+    execute_cmd(link_cmd)
     pass
 
 def make_static_lib(obj_list,target_name):
@@ -76,13 +72,13 @@ def make_static_lib(obj_list,target_name):
 
     ar_cmd="ar cr %s/lib%s.a %s" % (lib_dir,target_name,objects_tag)
     print(ar_cmd)    
-    linux_cmd(ar_cmd)
+    execute_cmd(ar_cmd)
     pass
 def make_share_lib(obj_list,target_name):
     objects_tag = " ".join(obj_list)
     share_cmd="%s -shared -fPCI -o %s/lib%s.so %s" % (cc,lib_dir,target_name,objects_tag)
     print(share_cmd)    
-    linux_cmd(share_cmd)
+    execute_cmd(share_cmd)
     pass
 
 def dir_recursion(dir_path,file_handle_func):
@@ -108,21 +104,12 @@ def target_execute_recursion(target_executing,targets):
     target_name = target_executing.getAttribute("name")
     print("\nexecuting target %s " % target_name)
 
-    children_ele=target_executing.getchildren()
-    for child_ele in children_ele:
-        ele_tag = child_ele.tagName
-        # TODO 做一个函数字典，自动执行 
-        if ele_tag == "config":
-
-
-    make_ele = target_executing.getElementsByTagName("make")[0]
-    dependences = make_ele.getAttribute("dependences")
+    dependences = target_executing.getAttribute("dependences")
     print("found dependence %s" % dependences)
     dependence_list = [] if dependences is None or len(dependences.strip())==0 else dependences.split(",")
     for dependence in dependence_list:
         if targets.get(dependence) is None:
             continue
-
         static_lib =os.path.join(lib_dir,"lib%s.a" % dependence)
         share_lib =os.path.join(lib_dir,"lib%s.so" % dependence)
         print("detecting dependence lib %s or %s" %(static_lib,share_lib))
@@ -131,51 +118,63 @@ def target_execute_recursion(target_executing,targets):
             continue
         target_execute_recursion(targets[dependence],targets)
 
-    object_list = []
-    def compile_and_collect_objects(src_file,src_dir):
-        compile_phase(src_file,src_dir)
-        object_list.append("%s/%s.obj" %(obj_dir,src_file))
+    src_dir=target_executing.getAttribute("src-dir")
 
-    compile_ele=target_executing.getElementsByTagName("compile")[0]
-    src_dir=compile_ele.getAttribute("src-dir")
-    src_files = compile_ele.getAttribute("src-files")
+    src_files = target_executing.getAttribute("src-files")
     src_files = [] if len(src_files.strip())==0 else src_files.split(",")
-
     if len(src_files) ==1 and src_files[0].startswith("*"):
         src_list=filter(lambda ele: os.path.isfile(ele),[ os.path.join(src_dir,ele) for ele in os.listdir(src_dir)])
     else:
         src_list=src_files
-    for src_file in src_list:
-        compile_and_collect_objects(src_file,src_dir)
 
-    sub_dirs = compile_ele.getAttribute("sub-dirs")
+    sub_dirs = target_executing.getAttribute("sub-dirs")
     sub_dirs = [] if len(sub_dirs.strip())==0 else sub_dirs.split(",")
     if len(sub_dirs) ==1 and sub_dirs[0].startswith("*"):
         sub_dirs=filter(lambda ele: os.path.isdir(ele),os.listdir(src_dir))
+
+    children_ele=[type(child)==xml.dom.minidom.Element for child in target_executing.childNodes]
+    for child_ele in children_ele:
+        print(child_ele)
+        ele_tag = child_ele.tagName
+        phase_function = function_mapping[ele_tag]
+        phase_function(child_ele,src_dir,src_list,sub_dirs)
+
+def compile_phase(compile_ele,src_dir,src_files,sub_dirs):
+
+    def compile(_src_file_src_dir):
+        compile_dir = os.path.join(obj_dir,_src_dir)
+        if not  os.path.exists(compile_dir):
+            dir_make(obj_dir,_src_dir)
+
+        compile_cmd="%s %s %s %s %s %s -c %s -o %s/%s.obj" % (cc,includes_tag,warn_tag,debug_tag,macros_tag,std_tag,_src_file,obj_dir,_src_file)
+        print(compile_cmd)
+        execute_cmd(compile_cmd)
+        pass
+
+    for src_file in src_list:
+        compile(src_file,src_dir)
     for sub_dir in sub_dirs:
-        dir_recursion(sub_dir,compile_and_collect_objects)
+        dir_recursion(sub_dir,compile)
 
-    make_type = make_ele.getAttribute("type")
-    make_name = make_ele.getAttribute("name")
-    make_phase(object_list,make_name,make_type,dependence_list)
-
-
-def compile_phase(src_file,src_dir):
-    includes_tag = " ".join(include_list)
-    macros_tag="  ".join(macro_list)
-
-    compile_dir = os.path.join(obj_dir,src_dir)
-    if not  os.path.exists(compile_dir):
-        dir_make(obj_dir,src_dir)
-
-    compile_cmd="%s %s %s %s %s %s -c %s -o %s/%s.obj" % (cc,includes_tag,warn_tag,debug_tag,macros_tag,std_tag,src_file,obj_dir,src_file)
-    print(compile_cmd)
-    linux_cmd(compile_cmd)
-
-def make_phase(objects,target_name,target_type,target_dependences):
+def make_phase(make_ele,src_dir,src_files,sub_dirs):
+    target_type = make_ele.getAttribute("type")
     if target_type not in ["exe","static","share"]:
         print("target must be any of [exe,static,share]")
         return 
+
+    objects=[]
+    def collect_objects(sub_file):
+        objects.append(sub_file)
+        
+    result_dir=os.path.join(obj_dir,src_dir)
+    dir_recursion(result_dir,collect_objects)
+
+    target_name = make_ele.getAttribute("name")
+    target_dependences = make_ele.getAttribute("libs")
+    target_dependences = [] if target_dependences is None or len(target_dependences.strip())==1 else target_dependences.split(",")
+    target_lib_dirs = make_ele.getAttribute("lib-dirs")
+    target_lib_dirs=[] if target_lib_dirs is None or len(target_lib_dirs.strip) == 0 else target_lib_dirs.split(",")
+
     if target_type == "exe":
         make_executable(objects,target_name,target_dependences,[lib_dir])
     elif target_type == "static":
@@ -209,10 +208,17 @@ if __name__ == "__main__":
         macros=macros.split(",")
     macro_list=["-D%s" % m for m in macros]
 
+    function_mapping["compile"]=compile_phase
+    function_mapping["make"]=make_phase
+    function_mapping["replace"]=config_phase
+
+
     target_dict = dict()
     targets = build_context.getElementsByTagName("target")
     for target in targets:
         target_dict[target.getAttribute("name")]=target
     target_execute_recursion(target_dict["all"],target_dict)
-    pass
+    
+
+    
 
